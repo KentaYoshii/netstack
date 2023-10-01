d891719d182989485ca835f24904a04018856657
Kenta Yoshii <kenta_yoshii@brown.edu>
2023-10-01 02:39:40 +0000

    add parser

--- cmd/vrouter/main.go
+++ cmd/vrouter/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+	"netstack/pkg/lnxconfig"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	if len(os.Args) != 3 {
+		log.Fatalf("Usage:  %s --config <linksfile>\n", os.Args[0])
+	}
+	// Parse the file
+	lnxConfig, err := lnxconfig.ParseConfig(os.Args[2])
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(lnxConfig.Interfaces[0].AssignedIP.String())
+	for {
+
+	}
 }
